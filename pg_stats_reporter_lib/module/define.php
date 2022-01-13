3068eefe7fd12045379bbb74d346e64b6b2fe308
UCHIDA Hiroyuki <uchida.hiroyuki@ntt-tx.co.jp>
2022-01-13 11:48:09 +0900

    Update jQuery, jQueryUI,Smarty

--- pg_stats_reporter_lib/module/define.php
+++ pg_stats_reporter_lib/module/define.php
@@ -12,9 +12,9 @@ define("PROGRAM_VERSION", "14.0");
 define("IMAGE_FILE", "pgsql_banner01.png");
 
 // Library path
-define("SMARTY_PATH", "package/smarty-3.1.35/libs/");
-define("JQUERY_PATH", "package/jquery-3.5.1.min.js");
-define("JQUERYUI_PATH", "package/jquery-ui-1.12.1.custom/");
+define("SMARTY_PATH", "package/smarty-3.1.43/libs/");
+define("JQUERY_PATH", "package/jquery-3.6.0.min.js");
+define("JQUERYUI_PATH", "package/jquery-ui-1.13.0.custom/");
 define("TIMEPICKER_PATH", "package/jquery-ui-timepicker-addon-1.6.3/");
 define("TABLESORTER_PATH", "package/tablesorter-2.31.3/");
 define("SUPERFISH_PATH", "package/superfish-1.7.10/dist/");
